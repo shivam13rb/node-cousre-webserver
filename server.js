42026c97f274a1f75bcc0f1235359e11f0002e1e
shivam13rb <shivam13rb@gmail.com>
2018-06-25 13:59:26 +0530

    Setuo start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+const port = process.env.PORT || 3000;
+
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -59,6 +61,6 @@ app.get('/bad',(req,res) => {
         errorMessage:'unable to handle request'
     });
 })
-app.listen(3000,() => {
-    console.log('server is up on port number 3000');
+app.listen(port,() => {
+    console.log(`server is up on port number ${port}`);
 });
